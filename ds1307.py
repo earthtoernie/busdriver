d69248bbbdd00f2e607e92fea69571d173ba2289
ernie <ernie.resume.reply@gmail.com>
2018-01-21 15:44:20 -0500

    now prints time in a loop

--- ds1307.py
+++ ds1307.py
@@ -30,7 +30,8 @@ def read_write_string(sp):
     payload = lib.i2c_utils.gen_read_from_reg(ds1207_read_addr, ds1207_start_ram_reg, 5)
     ret = lib.bp_serial_utils.write_bytes(sp, payload, .1)
     # print('********** payload is: {}'.format(payload))
-    print(ret)  # should be none, depending on how long we wait
+    # print(ret)  # should be none, depending on how long we wait
+    return ret
 
 
 
@@ -46,6 +47,7 @@ def write_time(sp, arg):
 
 
 def read_time(sp):
+    read_len = 7
     # just write the pointer to that register
     payload = lib.i2c_utils.gen_write_addr_only(ds1207_write_addr, ds1207_start_timedate_reg)
     ret = lib.bp_serial_utils.write_bytes(sp, payload, .1)
@@ -53,11 +55,11 @@ def read_time(sp):
     # print(ret) # should be none, depending on how long we wait
 
     # now read
-    payload = lib.i2c_utils.gen_read_from_reg(ds1207_read_addr, ds1207_start_timedate_reg, 7)
+    payload = lib.i2c_utils.gen_read_from_reg(ds1207_read_addr, ds1207_start_timedate_reg, read_len)
     ret = lib.bp_serial_utils.write_bytes(sp, payload, .1)
     # print('********** payload is: {}'.format(payload))
     # print(ret)  # should be none, depending on how long we wait
-    return ret
+    return (ret, ret[len(ret) - read_len:])
 
 
 if __name__ == '__main__':
@@ -66,16 +68,21 @@ if __name__ == '__main__':
     # read_write_string(sp)
 
     sp = lib.bp_serial_utils.init_to_i2c()
-    the_time = datetime.datetime(2003, 8, 4, 13, 30, 45)
+
     # Mon Aug 4 13:30:45 2003
+    # the_time = datetime.datetime(2003, 8, 4, 13, 30, 45)
     # the_time = datetime.datetime.now()
-    ret = write_time(sp, the_time)
-    print(ret)
+    # ret = write_time(sp, the_time)
 
-    time.sleep(3)
+    time.sleep(1)
 
-    read_time_junk = read_time(sp)
-    print(read_time_junk)
+
+    while True:
+        read_statusdata, read_data = read_time(sp)
+        time_stamp = lib.rtc_utils.gen_datetime([bytes([i]) for i in list(read_data)])
+        # print(read_data)
+        print(time_stamp)
+        time.sleep(1)
     # print(read_time().strftime("%a %b %-d %H:%M:%S %Y"))
 
     # the_time = datetime.datetime(2003, 8, 4, 13, 30, 45)
